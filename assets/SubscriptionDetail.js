a7c5895d750a788500c950fbdd401fbdec16a867
cb-naveenkumarsunkara <naveenkumar.sunkara@chargebee.com>
2022-05-13 13:06:11 +0530

    asdfadf

--- assets/SubscriptionDetail.js
+++ assets/SubscriptionDetail.js
@@ -1,1 +1,1 @@
-import{_ as e}from"./plugin-vue_export-helper.21dcd24c.js";import{o as r,c as t}from"./index.a06acbf2.js";const c={};function o(a,n){return r(),t("div",null," Detail ")}var _=e(c,[["render",o]]);export{_ as default};
+import{_ as e}from"./plugin-vue_export-helper.js";import{o as r,c as t}from"./index.js";const c={};function o(a,n){return r(),t("div",null," Detail ")}var _=e(c,[["render",o]]);export{_ as default};
