02854e4d744978aa877b8752d240f8f30815c3fa
cb-naveenkumarsunkara <naveenkumar.sunkara@chargebee.com>
2022-06-22 11:49:11 +0530

    asdfdsaf

---
+++ CreateHeader.js
@@ -0,0 +1,1 @@
+import{_ as t}from"./plugin-vue_export-helper.js";import{d as a,f as r,g as s,t as _}from"./index.js";const o={class:"c-shopify-header"},c=a({props:{title:null},setup(e){return(n,p)=>(r(),s("div",o,_(e.title),1))}});var l=t(c,[["__scopeId","data-v-b93e9070"]]);export{l as C};
